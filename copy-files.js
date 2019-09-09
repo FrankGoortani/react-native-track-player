3d8ace9da5baf261a25166698eb8b4f3ef4594a2
Frank Goortani <FRANK_GOORTANI@homedepot.com>
2019-09-08 23:38:34 -0400

    Add proper copy utility

---
+++ copy-files.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const fs = require('fs-extra')
+const rimraf = require('rimraf')
+
+const srcPath = path.resolve(__dirname, '.')
+const destFolder = path.resolve(__dirname, '../maktub/node_modules/react-native-track-player')
+
+rimraf(destFolder, function() {
+  console.log('all cleaned up!')
+  fs.copy(srcPath, destFolder, function(err) {
+    if (err) {
+      return console.error(err)
+    }
+    console.log('succesfully copied track player to maktub')
+  })
+})
